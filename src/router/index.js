b9f81870267e2ee5f73865d61cb5452df331d2a3
RobertLavrukevich <robertlavruk@gmail.com>
2025-05-12 21:22:12 +0300

    вторичный файлы done

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../components/Login.vue';
+import Chat from '../components/Chat.vue';
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/chat', component: Chat }
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+});
+
 
